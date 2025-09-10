e9c245ce3dd9105a6b415d776d088396d0f42169
ousam713 <ousambenz007@gmail.com>
2025-09-10 23:23:37 +0100

    first commit in task 1

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		do {
+			System.out.println( "==============\n"+
+								"Presse a number:\n"+
+							   "1. Show all student"+
+							   "2. \n"+
+							   "3. \n"+
+							   "4. \n"+
+							   "5. \n"+
+							   "=============="
+					);
+			
+			} while(true);
+ 	}
+
+}
