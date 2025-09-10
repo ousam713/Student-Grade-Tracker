99bff9c1809a3b2335aae8a18a56cfd31e097e1e
ousam713 <ousambenz007@gmail.com>
2025-09-10 23:48:28 +0100

    add main manu

--- Main.java
+++ Main.java
@@ -1,19 +1,62 @@
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		do {
-			System.out.println( "==============\n"+
-								"Presse a number:\n"+
-							   "1. Show all student"+
-							   "2. \n"+
-							   "3. \n"+
-							   "4. \n"+
-							   "5. \n"+
-							   "=============="
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		
+		do 
+		{
+			System.out.println( "/n=============="+
+								"\nPresse a number:"+
+							    "\n1. Manage students"+
+							    "\n2. Show students"+
+							    "\n3. Input grades"+
+							    "\n4. Manage grades"+
+							    "\n0. exit"+
+							    "/n=============="
 					);
+			
+				int n = in.nextInt();
+				
+				if(n == 0) break;
+				
+				switch(n) 
+				{
+					case 1:
+						do 
+						{
+							System.out.println( "/n=============="+
+									   "\nPresse a number:"+
+									   "\n1. Show student"+
+									   "\n2. Create students"+
+									   "\n3. Update student"+
+									   "\n4. Delete student"+
+									   "\n0. exit"+
+									   "/n=============="
+									);
+							int n1 = in.nextInt();
+							if(n1 == 0) break;
+						}while(true);
+						break;
+					case 2:
+						break;
+					case 3:
+						break;
+					case 4:
+						break;
+					default:
+						break;
+				}
 			
 			} while(true);
  	}
 
 }
+
+
+//"\n1. "+
+//"\n2. "+
+//"\n3. "+
+//"\n4. "+
