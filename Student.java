77c89685f96cd7b0ba8ae2b68121090c24415f85
ousam713 <ousambenz007@gmail.com>
2025-09-11 23:31:59 +0100

    implement createStd method

--- Student.java
+++ Student.java
@@ -1,8 +1,11 @@
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+
 
 public class Student 
-{
+{	
 	private static int count = 1;
 	private String id;
 	private String firstName;
@@ -11,12 +14,39 @@ public class Student
 	private String [] subjects = {"Programming with Java","Mathematics","English"};
 	private float avg;
 	
+	static Scanner in = new Scanner(System.in);
+	
 	public Student(String firstName,String lastName)
 	{
 		this.id = String.format("%08d", count);
 		this.firstName = firstName;
 		this.lastName = lastName;
 		count++;
+	}
+
+	public static void CreateStd() 
+	{
+		try 
+		{
+			System.out.print("enter first name : ");
+			String fName = in.nextLine();
+			
+			System.out.print("enter first name : ");
+			String lName = in.nextLine();
+			
+		} catch(Exception e){
+			System.out.println("");
+		}
+	}
+
+	public static void UpdateStd() 
+	{
+		
+	}
+
+	public static void DeleteStd() 
+	{
+		
 	}
 	
 	
